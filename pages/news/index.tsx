8986ae9b6902a7376645bb60f6fb941d3ed79878
Han Byul <yahb9@naver.com>
2023-05-09 15:37:57 +0900

    Feat: SSR by using getServerSideProps

---
+++ pages/news/index.tsx
@@ -0,0 +1,33 @@
+interface NewsProps {
+  newsData: any
+}
+
+const News = ({ newsData }: NewsProps) => {
+  return (
+    <>
+      <div>News letter hello</div>
+      <ul>
+        {newsData.map((news: any, i: number) => {
+          return (
+            <li key={i}>
+              {news.id} {news.title} {news.category}
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default News
+
+export const getServerSideProps = async () => {
+  const response = await fetch('http://localhost:4000/news')
+  const data = await response.json()
+
+  return {
+    props: {
+      newsData: data,
+    },
+  }
+}
